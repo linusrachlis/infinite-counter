5722737b3d0e8c86d068a99f69ca1f3feb28f731
Linus Rachlis <linus@rachlis.net>
2026-03-15 17:44:45 -0400

    feat: speed up for long transitions

--- src/script.ts
+++ src/script.ts
@@ -16,16 +16,21 @@ document.addEventListener("DOMContentLoaded", function() {
     let transitionalValue: number | null = null;
 
     /**
-     * Gets unset when timeout cleared
+     * Gets unset when timeout cleared. Initially null.
      */
     let transitionTimeoutId: number | null = null;
 
-    const transitionStepDelayMs = 300;
-    const middleTransitionStepDelayMs = 50;
-    let shouldCount = false;
-    const initialDelayMs = 600;
-    const delayDecayFactor = 0.8;
-    const minDelayMs = 50;
+    // Related to transition animations
+    const transitionFirstOrLastStepDelayMs = 400;
+    const transitionStepInitialDelayMs = 100;
+    const transitionStepDelayDecayFactor = 0.95;
+    const transitionStepMinDelayMs = 10;
+
+    // Related to when you hold down the pointer on one of the GUI buttons
+    let pointerHoldShouldCount = false;
+    const pointerHoldInitialDelayMs = 600;
+    const pointerHoldDelayDecayFactor = 0.8;
+    const pointerHoldMinDelayMs = 50;
 
     const counterContainer = document.getElementById("counter-container");
     const plusButton = <HTMLButtonElement>document.getElementById("button_plus");
@@ -49,8 +54,16 @@ document.addEventListener("DOMContentLoaded", function() {
     /**
         Take transitionalValue one more step in the direction of the currentValue,
         and animate.
+
+        @param isFirstStep
+        @param adjustedDelayMs If this is a middle step (neither the first nor
+        the last), this is used as the duration for the animation and the delay
+        before the next step.
     */
-    function stepTowardsCurrentValue(isFirstStep: boolean): void {
+    function stepTowardsCurrentValue(isFirstStep: boolean, adjustedDelayMs: number): void {
+        // TODO: use an ease-in-out function for the step delays on long
+        // transitions so it also slows down towards the end
+
         // TODO: handle negative sign separately when number length is
         // changing (maybe have it slide over)
 
@@ -65,7 +78,7 @@ document.addEventListener("DOMContentLoaded", function() {
         } else if (transitionalValue > currentValue) {
             nextValue = transitionalValue - 1;
         } else {
-            // Shoudn't happen, just for robustness
+            // Shouldn't happen, just for robustness
             clearTimeout(transitionTimeoutId);
             transitionTimeoutId = null;
             return;
@@ -115,41 +128,54 @@ document.addEventListener("DOMContentLoaded", function() {
 
         const animationClassPrefix = nextValue > transitionalValue ? "increment" : "decrement";
         const isLastStep = nextValue === currentValue;
-        const additionalClassNames: string[] = [];
 
-        if (isFirstStep && isLastStep) {
-            additionalClassNames.push("only-step");
-        } else if (isFirstStep) {
-            additionalClassNames.push("first-step");
-        } else if (isLastStep) {
-            additionalClassNames.push("last-step");
-        } else {
-            additionalClassNames.push("middle-step");
-        }
+        // Default to current delay
+        let nextDelayMs: number = adjustedDelayMs;
 
         const digitsEnteringElem = createElementWithTextContent("div", digitsEntering.join(""));
-        digitsEnteringElem.classList.add(
-            `${animationClassPrefix}-entering`,
-            ...additionalClassNames
-        );
-
+        digitsEnteringElem.classList.add(`${animationClassPrefix}-entering`);
         const digitsStayingElem = createElementWithTextContent("div", digitsStaying.join(""));
-
         const digitsLeavingElem = createElementWithTextContent("div", digitsLeaving.join(""));
-        digitsLeavingElem.classList.add(
-            `${animationClassPrefix}-leaving`,
-            ...additionalClassNames
-        );
+        digitsLeavingElem.classList.add(`${animationClassPrefix}-leaving`);
+
+        function doForAnimatingDigitGroups(callback: (elem: HTMLElement) => void) {
+            for (const elem of [digitsEnteringElem, digitsLeavingElem]) {
+                callback(elem);
+            }
+        }
+
+        if (isFirstStep && isLastStep) {
+            doForAnimatingDigitGroups(elem => {
+                elem.classList.add("only-step");
+            });
+        } else if (isFirstStep) {
+            doForAnimatingDigitGroups(elem => {
+                elem.classList.add("first-step");
+            });
+        } else if (isLastStep) {
+            doForAnimatingDigitGroups(elem => {
+                elem.classList.add("last-step");
+            });
+        } else {
+            // If this is a middle step, decrease the delay
+            nextDelayMs = Math.max(transitionStepMinDelayMs, adjustedDelayMs * transitionStepDelayDecayFactor);
+
+            doForAnimatingDigitGroups(elem => {
+                elem.classList.add("middle-step");
+                elem.style.animationDuration = `${adjustedDelayMs}ms`;
+            });
+        }
 
         counterContainer.replaceChildren(digitsEnteringElem, digitsStayingElem, digitsLeavingElem);
 
         transitionalValue = nextValue;
         if (transitionalValue !== currentValue) {
-            const stepDelay = (isFirstStep || isLastStep) ? transitionStepDelayMs : middleTransitionStepDelayMs;
+            const stepDelay = (isFirstStep || isLastStep) ? transitionFirstOrLastStepDelayMs : adjustedDelayMs;
             transitionTimeoutId = setTimeout(
                 stepTowardsCurrentValue,
                 stepDelay,
-                false
+                false,
+                nextDelayMs
             );
         }
     }
@@ -157,7 +183,7 @@ document.addEventListener("DOMContentLoaded", function() {
     function updateCurrentValue(newValue: number): void {
         currentValue = newValue;
         if (transitionTimeoutId) clearTimeout(transitionTimeoutId);
-        stepTowardsCurrentValue(true);
+        stepTowardsCurrentValue(true, transitionStepInitialDelayMs);
     }
 
     function addToValue(mult: number): void {
@@ -172,42 +198,42 @@ document.addEventListener("DOMContentLoaded", function() {
         updateCurrentValue(Math.round(currentValue / getIncrementValue()));
     }
 
-    function startCounting(callback: () => void): void {
-        shouldCount = true;
-        count(callback, initialDelayMs);
+    function pointerHoldStart(callback: () => void): void {
+        pointerHoldShouldCount = true;
+        pointerHoldStep(callback, pointerHoldInitialDelayMs);
     }
 
-    function count(callback: () => void, delayMs: number): void {
-        if (!shouldCount) return;
+    function pointerHoldStep(callback: () => void, delayMs: number): void {
+        if (!pointerHoldShouldCount) return;
 
         callback();
-        const nextDelayMs = Math.max(minDelayMs, delayMs * delayDecayFactor);
-        setTimeout(count.bind(null, callback, nextDelayMs), delayMs);
+        const nextDelayMs = Math.max(pointerHoldMinDelayMs, delayMs * pointerHoldDelayDecayFactor);
+        setTimeout(pointerHoldStep.bind(null, callback, nextDelayMs), delayMs);
     }
 
-    function stopCounting(): void {
-        shouldCount = false;
+    function pointerHoldStop(): void {
+        pointerHoldShouldCount = false;
     }
 
     document.addEventListener("pointerdown", function(e) {
         switch (e.target) {
             case plusButton:
-                startCounting(addToValue.bind(null, 1));
+                pointerHoldStart(addToValue.bind(null, 1));
                 break;
             case minusButton:
-                startCounting(addToValue.bind(null, -1));
+                pointerHoldStart(addToValue.bind(null, -1));
                 break;
             case timesButton:
-                startCounting(multiplyValue);
+                pointerHoldStart(multiplyValue);
                 break;
             case divideButton:
-                startCounting(divideValue);
+                pointerHoldStart(divideValue);
                 break;
         }
     });
 
-    document.addEventListener("pointerup", function(e) { stopCounting(); });
-    document.addEventListener("pointercancel", function(e) { stopCounting(); });
+    document.addEventListener("pointerup", function() { pointerHoldStop(); });
+    document.addEventListener("pointercancel", function() { pointerHoldStop(); });
 
     document.addEventListener("keydown", function(e) {
         switch (e.key) {
